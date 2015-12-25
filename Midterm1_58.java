d42916dec1b509a949124d73f46852c028dd6633
thrbs <bancyo55@hotmail.co.th>
2015-12-25 17:10:38 +0700

    Update Midterm1_58.java

--- Midterm1_58.java
+++ Midterm1_58.java
@@ -1,4 +1,3 @@
-package midtem1_58;
 
 public class Midtem1_58 {
 
@@ -19,5 +18,57 @@ public class Midtem1_58 {
     
     public static void display(MobilePhone m){
         System.out.println(m.getBrand() + " series " + m.getSeries() + " is owned by " + m.getOwner().getName());
+    }
+}
+
+
+
+public class Student {
+    private String name;
+    private int age;
+    
+    Student(String name){
+        this.name = name;
+    }
+    Student(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    
+    public void setAge(int age){
+        this.age = age;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
+
+
+
+public class MobilePhone {
+    private String brand;
+    private String series;
+    private Student s;
+    
+    MobilePhone(String brand, String series){
+        this.brand = brand;
+        this.series = series;
+    }
+    
+    public void setOwner(Student s){
+        this.s = s;
+    }
+    public Student getOwner(){
+        return this.s;
+    }
+    
+    public String getBrand(){
+        return this.brand;
+    }
+    public String getSeries(){
+        return this.series;
     }
 }
